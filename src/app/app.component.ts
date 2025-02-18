69a286c8a5efcce5ff4ff696a5f875e6776c85d9
Remi <gabrielwillian.limari@elorrieta-errekamari.com>
2025-02-18 22:55:17 +0100

    Cambio landingpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import * as mapboxgl from 'mapbox-gl';
 import { LocalesTarjetaComponent } from "./pages/locales/locales-tarjeta/locales-tarjeta.component";
@@ -10,34 +10,38 @@ import { AuthService } from './services/auth.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { LocalService } from './services/local.service';
 import { Local } from './interfaces/local';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TranslateModule, NavComponent, RouterModule, NavComponent],
+  imports: [RouterOutlet, CommonModule, TranslateModule, NavComponent, RouterModule, NavComponent,
+    FormsModule,MatSidenavModule,MatToolbarModule,
+    MatButtonModule,MatIconModule,MatListModule,RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent   {
   title = 'proyecto';
-  map!: mapboxgl.Map;
-  marcadores: any[] = [];
-
-  locals: Local[] = [];  
-  usuarios: any[] = []; 
-
-  constructor(
-    private router: Router, 
-    private serviceDataService: LocalService, 
-    private authService: AuthService) {}
-
-  ngOnInit() {
-
-    this.serviceDataService.getLocales().subscribe((data) => {
-      this.locals = data;
-    });
+ 
+  constructor( private router: Router, ) {}
+  dropdownVisible: boolean = false;
+  mitranslate: TranslateService = inject(TranslateService);
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
   }
+
+  translateText(lang: string) {
+    this.mitranslate.use(lang);
+  }
+
 
   /* Para detectar si estoy en la página de login */
   isLoginPage(): boolean {
